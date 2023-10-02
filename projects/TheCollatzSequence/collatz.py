9444a96e0b68e9305e0f6aa2ce3cb7569938a820
nicole.garrow64@gmail.com <ngarrow@paystone.com>
2023-10-02 15:09:31 -0400

    Added user input and retry until 1 functionality

--- projects/TheCollatzSequence/collatz.py
+++ projects/TheCollatzSequence/collatz.py
@@ -1,14 +1,14 @@
 def collatz(number):
-    #if number even --> print number // 2 and return value of number // 2
-    if number % 2 == 0:
-        number = number // 2
+    while(number != 1):
+        #if even
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
 
-    #if odd --> print and return 3 * number + 1
-    if number % 2 == 1:
-        number = (number * 3) + 1
+        #if odd
+        else:
+            number = (number * 3) + 1
+            print(number)
 
-    print(number)
-
-
-
-collatz(2)
+number = int(input("Enter a number: "))
+collatz(number)
